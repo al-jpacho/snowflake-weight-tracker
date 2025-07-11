49b698bf30638efaed318d1e3e7e7aa611f9422a
al-jpacho <jordanapacho@gmail.com>
2025-07-11 17:01:40 +0100

    fix(snowflake): add the domain annd schema to weight_logs_raw table

--- snowflake_setup/create_weight_logs_raw.sql
+++ snowflake_setup/create_weight_logs_raw.sql
@@ -1,6 +1,7 @@
-create or replace table weight_logs_raw (
+create or replace table WEIGHT_DB.RAW.weight_logs_raw (
     id string, -- Notino page ID
     date date, -- Date of the weight log
     weight_kg float, -- Weight in kilograms
-    time_of_day string -- Time of day when the weight was recorded (e.g., "morning", "evening")
+    time_of_day string, -- Time of day when the weight was recorded (e.g., "morning", "evening")
+    loaded_at timestamp_ltz default current_timestamp() -- Timestamp when the record was loaded to DB
 )
