225be1af9492abed0de418149212210942561070
al-jpacho <jordanapacho@gmail.com>
2025-08-03 16:19:23 +0100

    feat(marts): add model for fact table for weight logs

---
+++ weight_tracker/models/marts/fct_weight_logs.sql
@@ -0,0 +1,14 @@
+with source as (
+    select * from {{ ref('int_weight_logs') }}
+)
+select
+    id,
+    weigh_in_date as date,
+    time_of_day,
+    day_of_week,
+    week_start_date as week,
+    month_start_date as month,
+    year_start_date as year,
+    quarter_start_date as quarter
+    weight_kg
+from source
