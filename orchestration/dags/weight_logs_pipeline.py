64987fb826b50fee35b4ce402647d3096428838b
al-jpacho <jordanapacho@gmail.com>
2025-08-04 21:55:08 +0100

    feat(DAG): add default args for airflow — retry 5 mins

--- orchestration/dags/weight_logs_pipeline.py
+++ orchestration/dags/weight_logs_pipeline.py
@@ -38,3 +38,11 @@ def extract_rows(results):
         rows.append(row)
 
     return pd.DataFrame(rows)
+
+
+# Airflow DAG
+default_args = {
+    "owner": "airflow",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
